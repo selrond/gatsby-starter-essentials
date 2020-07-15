a85ee3e33d36673e5cb10c297c560cdff001053f
Sebastian Andil <sebastian.andil@gmail.com>
2020-07-15 20:29:13 +0200

    Add explanatory comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+/**
+ * Set up absolute imports - any import path is first assumed to come from 'src'
+ */
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
